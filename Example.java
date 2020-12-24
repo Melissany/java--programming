9a3c637a434efb4caa5196bd34b729deb11626eb
Melissany <76564838+Melissany@users.noreply.github.com>
2020-12-24 06:01:06 -1000

    Creating first file in github

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example{
+       public static void main( String[] args){
+       int a;
+       int b;
+       int sum = a+b;
+       system.out.println(sum);
+       
+       }
+
+
+}
